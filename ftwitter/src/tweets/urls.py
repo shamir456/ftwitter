45eaf3f779be192211d61b287dc4c1651422ae4a
moiz <k164024@gmail.com>
2019-05-01 01:13:04 -0700

    UI done

--- ftwitter/src/tweets/urls.py
+++ ftwitter/src/tweets/urls.py
@@ -8,9 +8,12 @@ from .views import (
 	TweetDeleteView)
 from django.conf import settings
 
+from django.views.generic.base import RedirectView
+
 urlpatterns = [
    
-    url(r'^$',TweetListView.as_view(),name='list'), #/tweets/
+   url(r'^$',RedirectView.as_view(url='/')),
+    url(r'^search/$',TweetListView.as_view(),name='list'), #/tweets/
     url(r'^create/$',TweetCreateView.as_view(),name='create'),
     url(r'^(?P<pk>\d+)/$',TweetDetailView.as_view(),name='detail'), #/tweet/1/detail
     url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),  #/tweet/1/update
