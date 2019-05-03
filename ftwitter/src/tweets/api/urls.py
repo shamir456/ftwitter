9a207eda42c08e7eef6e141cbe06df73df40b978
moiz <k164024@gmail.com>
2019-05-03 13:27:31 -0700

    auto search tk hogya ha

--- ftwitter/src/tweets/api/urls.py
+++ ftwitter/src/tweets/api/urls.py
@@ -6,6 +6,7 @@ from .views import (
 	# TweetListView,
 	# TweetCreateView,TweetUpdateView,
 	# TweetDeleteView
+	TweetCreateAPIView,
     TweetListAPIView
     )
 from django.conf import settings
@@ -16,7 +17,7 @@ urlpatterns = [
    
 #    url(r'^$',RedirectView.as_view(url='/')),
      url(r'^$',TweetListAPIView.as_view(),name='list'), #api/tweets/
-#     url(r'^create/$',TweetCreateView.as_view(),name='create'),
+     url(r'^create/$',TweetCreateAPIView.as_view(),name='create'),
 #     url(r'^(?P<pk>\d+)/$',TweetDetailView.as_view(),name='detail'), #/tweet/1/detail
 #     url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),  #/tweet/1/update
 #     url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'), #/tweet/1/delete
--- ftwitter/src/tweets/api/views.py
+++ ftwitter/src/tweets/api/views.py
@@ -1,13 +1,32 @@
 from rest_framework import generics
-
+from django.db.models import Q
+from rest_framework import permissions
 
 from tweets.models import Tweet
 from .serializers import TweetModelSerializer
 
+
+
+class TweetCreateAPIView(generics.CreateAPIView):
+	serializer_class= TweetModelSerializer
+	permission_classes = [permissions.IsAuthenticated ]
+
+	def perform_create(self,serializer):
+		serializer.save(user=self.request.user)
+
+
+
 class TweetListAPIView(generics.ListAPIView):
 	serializer_class= TweetModelSerializer
 
-	def get_queryset(self):
-		return Tweet.objects.all()
-
-
+	def get_queryset(self,*args,**kwargs):
+		qs=Tweet.objects.all().order_by("-timestamp")
+		
+		query=self.request.GET.get('q',None)
+		if query is not None:
+			qs=qs.filter(
+				Q(content__icontains=query) |
+				Q(user__username__icontains=query)
+				)
+		return qs
+		
